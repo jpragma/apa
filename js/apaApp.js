7574a4bcc4aa4fc607abeb13f2060cf25f3e56e3
Isaac Levin <isaac.levin@cibc.com>
2015-05-15 10:09:16 -0400

    Initial import

---
+++ js/apaApp.js
@@ -0,0 +1,222 @@
+var apaApp = angular.module('apaApp', ['ngRoute','ui.bootstrap']);
+apaApp.directive('onLongPress', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function($scope, $elm, $attrs) {
+            $elm.bind('touchstart', function (evt) {
+                $scope.longPress = true;
+                $timeout(function () {
+                    if ($scope.longPress) {
+                        $scope.$apply(function () {
+                            $scope.$eval($attrs.onLongPress)
+                        });
+                    }
+                }, 600);
+            });
+            $elm.bind('touchend', function (evt) {
+                $scope.longPress = false;
+                if ($attrs.onTouchEnd) {
+                    $scope.$apply(function () {
+                        $scope.$eval($attrs.onTouchEnd)
+                    });
+                }
+            });
+        }
+    }
+});
+
+var Command = function(execute, undo) {
+    this.execute = execute;
+    this.undo = undo;
+};
+
+var addPoints = function (game, command, balls) {
+    var points = 0;
+    for (var i=0; i<balls.length; i++) {
+        points += (balls[i] == 9) ? 2 : 1;
+        game.curRack.balls[balls[i]] = (game.curPlayerIdx + 1); // mark ball that is gone
+    }
+    game.curRack.rackScore[game.curPlayerIdx] += points;
+    game.score[game.curPlayerIdx] += points;
+};
+var removePoints = function (game, command, balls) {
+    var points = 0;
+    for (var i=0; i<balls.length; i++) {
+        points += (balls[i] == 9) ? 2 : 1;
+        game.curRack.balls[balls[i]] = 0; // re-enable the ball
+    }
+    game.curRack.rackScore[game.curPlayerIdx] -= points;
+    game.score[game.curPlayerIdx] -= points;
+};
+var addDefenseIfNecessary = function(game, command, defense) {
+    command.defense = defense;
+    if (defense) {
+        game.defensiveShots[game.curPlayerIdx]++;
+    }
+};
+var removeDefenseIfNecessary = function(game, command) {
+    if (command.defense) {
+        game.defensiveShots[game.curPlayerIdx]--;
+    }
+};
+var MadeCommand = function(game, defense) {
+    var rack = game.curRack;
+    var execute = function() {
+        addDefenseIfNecessary(game, this, defense);
+        var balls = rack.thisInningMadeBalls();
+        this.balls = balls; // save for ref in undo
+        addPoints(game, this, balls);
+    };
+    var undo = function () {
+        removeDefenseIfNecessary(game, this);
+        var balls = this.balls;
+        removePoints(game, this, balls);
+    };
+    return new Command(execute, undo);
+};
+var MissedCommand = function(game, defense) {
+    var rack = game.curRack;
+    var execute = function() {
+        addDefenseIfNecessary(game, this, defense);
+        if (game.curPlayerIdx == 1) {
+            rack.inningNumber++;
+            this.inningAdded = true;
+        }
+        this.curPlayerIdx = game.curPlayerIdx;
+        game.curPlayerIdx = (game.curPlayerIdx * -1) + 1; // flip players
+        // Mark Dead Balls
+        this.deadBalls = rack.thisInningMadeBalls();
+        for (var i=0; i<this.deadBalls.length; i++) {
+            game.curRack.balls[this.deadBalls[i]] = (this.deadBalls[i] == 9) ? 0 : 99; // 9 is re-spotted
+        }
+    };
+    var undo = function () {
+        game.curPlayerIdx = this.curPlayerIdx;
+        removeDefenseIfNecessary(game, this);
+        if (this.inningAdded == true) {
+            rack.inningNumber--;
+        }
+        // Un-mark Dead Balls
+        for (var i=0; i<this.deadBalls.length; i++) {
+            game.curRack.balls[this.deadBalls[i]] = 0;
+        }
+    };
+    return new Command(execute, undo);
+};
+var ScratchCommand = function(game, defense) {
+    return new MissedCommand(game, defense);
+};
+var UpdateBallValueCommand = function (game, ballIdx, newValue) {
+    var rack = game.curRack;
+    var execute = function() {
+        this.oldValue = rack.balls[ballIdx]; // can be 1, 2, 99
+        handlePoints(game, ballIdx, this.oldValue, newValue);
+    };
+    var undo = function () {
+        handlePoints(game, ballIdx, newValue, this.oldValue);
+    };
+    var handlePoints = function(rack, ballIdx, oldValue, newValue) {
+        var rack = game.curRack;
+        var points = (ballIdx == 9) ? 2 : 1;
+        if (oldValue != 99) {
+            rack.rackScore[oldValue-1] -= points;
+            game.score[oldValue-1] -= points;
+        }
+        rack.rackScore[newValue-1] += points;
+        game.score[newValue-1] += points;
+        rack.balls[ballIdx] = newValue;
+    };
+    return new Command(execute, undo);
+};
+
+apaApp.config(function($routeProvider){
+    $routeProvider.
+        when('/9BallGameSetup', {templateUrl: '9bGameSetup.html', controller: '9bGameSetupController'}).
+        when('/9bRack', {templateUrl: '9bRack.html', controller: '9bRackController'}).
+        when('/9bResult', {templateUrl: '9bResult.html', controller: '9bResultController'}).
+        when('/', {templateUrl: 'splash.html'}).
+        otherwise({redirectTo: '/'});
+});
+
+apaApp.factory('Game9B', function() {
+    var game = {};
+    game.pointsToWin = [-1, 14, 19, 25, 31, 38, 46, 55, 65, 75];
+    game.scoreOfMatch = [null,
+        [0, 3, 4, 5, 7, 8, 9, 11, 12], // SL 1
+        [0, 4, 6, 8, 9, 11, 13, 15, 17], // SL 2
+        [0, 5, 7, 10, 12, 15, 17, 20, 22], // SL 3
+        [0, 6, 9, 12, 15, 19, 22, 25, 28], // SL 4
+        [0, 7, 11, 15, 19, 23, 27, 30, 34], // SL 5
+        [0, 9, 13, 18, 23, 28, 32, 37, 41], // SL 6
+        [0, 11, 16, 22, 27, 33, 38, 44, 50], // SL 7
+        [0, 14, 20, 27, 33, 40, 46, 53, 59], // SL 8
+        [0, 18, 25, 32, 39, 47, 54, 61, 68] // SL 9
+    ];
+    game.players = [{name:'Isaac', handicap: 4}, {name:'Dennis', handicap: 7}];
+    game.curPlayerIdx = 0;
+
+    game.init = function() {
+        game.startTime = new Date();
+        game.score = [0,0];
+        game.defensiveShots = [0,0];
+        game.totalInnings = 0;
+        game.curRack = null;
+    };
+
+    game.ptsToWin = function(playerIdx) {
+        return game.pointsToWin[game.players[playerIdx].handicap];
+    };
+
+    game.isOver = function() {
+        return game.score[0] >= game.ptsToWin(0) || game.score[1] >= game.ptsToWin(1);
+    };
+
+    game.ptsLeft = function (playerIdx) {
+        return game.ptsToWin(playerIdx) - game.score[playerIdx];
+    };
+
+    game.getWinner = function() {
+        var winner = null;
+        if (game.isOver()) {
+            winner = (game.score[0] >= game.ptsToWin(0)) ? game.players[0] : game.players[1];
+        }
+        return winner;
+    };
+
+    game.getMatchScorePoints = function(idx) {
+        var looserIdx = (game.score[0] >= game.ptsToWin(0)) ? 1 : 0;
+        var looserScoreLine = game.scoreOfMatch[game.players[looserIdx].handicap];
+        var looserPoints = 0;
+        for (var i = looserScoreLine.length - 1; i >= 0; i--) {
+            if (game.score[looserIdx] >= looserScoreLine[i]) {
+                looserPoints = i;
+                break;
+            }
+        }
+        return (looserIdx == idx) ? looserPoints : 20 - looserPoints;
+    };
+
+    game.startNextRack = function() {
+        var rack = {};
+        rack.number = (game.curRack == null) ? 1 : (game.curRack.number + 1);
+        rack.rackScore = [0,0];
+        rack.inningNumber = 0;
+        rack.balls = [null, 0,0,0,0,0,0,0,0,0]; // Values: -1=selected; 0=available; 1=made(1st player); 2=made(2nd player); 99=dead
+        rack.commands = [];
+        rack.thisInningMadeBalls = function () {
+            var result = [];
+            for (var i=1; i<rack.balls.length; i++) {
+                if (rack.balls[i] == -1)
+                    result.push(i);
+            }
+            return result;
+        };
+        rack.madeAnyBallThisInning = function () {
+            return (rack.thisInningMadeBalls().length > 0);
+        };
+        rack.breakerIdx = game.curPlayerIdx;
+        game.curRack = rack;
+    };
+
+    return game;
+});
---
+++ js/apaControllers.js
@@ -0,0 +1,210 @@
+apaApp.controller('9bGameSetupController', function ($scope, Game9B, $location) {
+    $scope.game = Game9B;
+    $scope.swapPlayers = function() {
+        var p = Game9B.players[0];
+        Game9B.players[0] = Game9B.players[1];
+        Game9B.players[1] = p;
+    };
+    $scope.startGame = function() {
+        Game9B.init();
+        Game9B.startNextRack();
+        $location.path("/9bRack");
+    };
+});
+
+
+apaApp.controller('9bRackController', function ($scope, Game9B, $location) {
+    if (!Game9B.curRack) {
+        $location.path("/");
+        return;
+    }
+
+    $scope.game = Game9B;
+    $scope.rack = Game9B.curRack;
+    $scope.scratch = false;
+    $scope.defense = false;
+    $scope.scoreDisplayType = 0;
+    $scope.changeDisplayType = function () {
+        if ($scope.scoreDisplayType == 2) {
+            $scope.scoreDisplayType = 0;
+        } else {
+            $scope.scoreDisplayType++;
+        }
+    };
+    $scope.toggleBallSelect = function(idx) {
+        if ($scope.rack.balls[idx] >= 1) {
+            // already gone
+            $scope.switchToOverride(idx);
+            return;
+        }
+        $scope.rack.balls[idx] = Math.abs($scope.rack.balls[idx]) - 1;
+    };
+    $scope.switchToOverride = function(idx) {
+        $scope.overrideMode = true;
+        $scope.overrideBall = {idx: idx, value: $scope.rack.balls[idx]};
+    };
+    $scope.getBtnStyleSelected = function (val) {
+        return (val == $scope.overrideBall.value) ? {opacity:0.3} : null;
+    };
+    $scope.updateBall = function (idx, value) {
+        var cmd = new UpdateBallValueCommand($scope.game, idx, value);
+        $scope.rack.commands.push(cmd);
+        cmd.execute();
+        $scope.overrideMode = false;
+    };
+
+    $scope.getBallText = function (idx) {
+        var txt = idx;
+        if ($scope.rack.balls[idx] == 99) {
+            txt += " (D)";
+        } else if ($scope.rack.balls[idx] >= 1) {
+            txt += " (" +  $scope.rack.balls[idx] + ")";
+        }
+        return txt;
+    };
+    $scope.getBallSelectClass = function(idx) {
+        var className = "ball" + idx;
+        if ($scope.rack.balls[idx] == -1) {
+            className += " ball-selected";
+        } else if ($scope.rack.balls[idx] >= 1) {
+            className += " ball-gone";
+        }
+
+        return className;
+    };
+    $scope.getScratchBtnClass = function () {
+        return 'btnScratchDefense' + (($scope.scratch) ? ' ball-selected' : '');
+    };
+    $scope.getDefenseBtnClass = function () {
+        return 'btnScratchDefense' + (($scope.defense) ? ' ball-selected' : '');
+    };
+    $scope.done = function () {
+        var cmd = null;
+        if ($scope.scratch) {
+            cmd = new ScratchCommand($scope.game, $scope.defense);
+            $scope.scratch = false;
+        } else {
+            if ($scope.rack.madeAnyBallThisInning()) {
+                cmd = new MadeCommand($scope.game, $scope.defense);
+            } else {
+                cmd = new MissedCommand($scope.game, $scope.defense);
+            }
+        }
+
+        $scope.rack.commands.push(cmd);
+        cmd.execute();
+        $scope.defense = false; // reset defense button
+        if ($scope.rack.balls[9] >= 1) {
+            $location.path("/9bResult");
+        }
+        if ($scope.game.isOver()) {
+            $location.path("/9bResult");
+        }
+    };
+    $scope.undo = function () {
+        var cmd = $scope.rack.commands.pop();
+        cmd.undo();
+    };
+
+});
+
+apaApp.controller('9bResultController', function ($scope, Game9B, $location) {
+    if (!Game9B.curRack) {
+        $location.path("/");
+        return;
+    }
+
+    $scope.game = Game9B;
+    $scope.rack = Game9B.curRack;
+
+    $scope.editRack = function () {
+        $location.path("/9bRack");
+    };
+
+    var markNotPocketedBallsAsDead = function() {
+        // all not pocketed balls become "dead"
+        for (var i=1; i < $scope.rack.balls.length; i++) {
+            if ($scope.rack.balls[i] == 0) {
+                $scope.rack.balls[i] = 99;
+            }
+        }
+    };
+    markNotPocketedBallsAsDead();
+
+    $scope.game.totalInnings += $scope.game.curRack.inningNumber;
+
+    var calcPointsMadeBy = function(playerIdx) {
+        var result = 0;
+        for (var i=1; i < $scope.rack.balls.length; i++) {
+            if ($scope.rack.balls[i] == [playerIdx]) {
+                result += (i == 9) ? 2 : 1;
+            }
+        }
+        return result;
+    };
+
+    $scope.pointsMade = [calcPointsMadeBy(1), calcPointsMadeBy(2)];
+    $scope.dead = calcPointsMadeBy(99);
+    $scope.origPointsMade = $scope.pointsMade.slice(0); // copy by value
+
+    // Adjust score if match is over and score is more than max
+    for (var i = 0; i <= 1; i++) {
+        var delta = $scope.game.ptsLeft(i);
+        if (delta < 0) {
+            $scope.game.score[i] += delta;
+            $scope.pointsMade[i] += delta;
+            $scope.dead -= delta;
+        }
+    }
+
+    $scope.isValid = function () {
+        return ($scope.pointsMade[0] + $scope.pointsMade[1] + $scope.dead) == 10;
+    };
+
+    $scope.breakAndRun = function () {
+        return $scope.rack.inningNumber == 0 && $scope.rack.breakerIdx == $scope.game.curPlayerIdx && $scope.dead == 0;
+    };
+
+    $scope.getAvgBallPerInning = function (playerIdx) {
+        var effectiveInnings = ($scope.game.totalInnings - $scope.game.defensiveShots[playerIdx]);
+        effectiveInnings = (effectiveInnings == 0) ? 1 : effectiveInnings;
+        return $scope.game.score[playerIdx] / (effectiveInnings);
+    };
+
+    $scope.getTotalTime = function () {
+        var totalMinutes =  (new Date() - $scope.game.startTime) / 1000 / 60;
+        var h = Math.floor(totalMinutes / 60);
+        var m = Math.floor(totalMinutes % 60);
+        var result = m + " minutes";
+        if (h > 0) {
+            result = h + " hour(s) " + result;
+        }
+        return result;
+    };
+
+    $scope.next = function () {
+        for (var i=0; i<=1; i++) {
+            if ($scope.origPointsMade[i] != $scope.pointsMade[i]) {
+                $scope.game.score[i] += ($scope.pointsMade[i] - $scope.origPointsMade[i]);
+            }
+        }
+        if ($scope.game.isOver()) {
+            $location.path("/9BallGameSetup");
+        } else {
+            $scope.game.startNextRack();
+            $location.path("/9bRack");
+        }
+    };
+});
+
+
+
+
+
+
+apaApp.controller('teamController', function ($scope, $http) {
+
+    $http.get('teams').success(function (data) {
+        $scope.teams = data;
+    });
+});
